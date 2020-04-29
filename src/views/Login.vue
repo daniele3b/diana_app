770481d9f426bed70f955ebfcb8099cedd659e1b
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-04-29 23:40:49 +0200

    Login modified

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,12 +8,12 @@
             <h5 class="card-title text-center"><b>Accedi</b></h5>
             <form class="form-signin">
               <div class="form-label-group">
-                <input type="text" id="inputEmailOrPhone" class="form-control" placeholder="Indirizzo email o telefono" required autofocus>
+                <input type="text" id="inputEmailOrPhone" class="form-control" v-model="emailOrPhone" placeholder="Indirizzo email o telefono" required autofocus>
                 <label for="inputEmail">Indirizzo email o telefono</label>
               </div>
 
               <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <input type="password" id="inputPassword" class="form-control" v-model="password" placeholder="Password" required>
                 <label for="inputPassword">Password</label>
               </div>
 
@@ -21,6 +21,8 @@
                 <input type="checkbox" class="custom-control-input" id="customCheck1">
                 <label class="custom-control-label" for="customCheck1">Mantieni l'accesso</label>
               </div>
+              readyEMail : {{readyEmail}} readyPhone : {{readyPhone}}
+              {{emailOrPhone}} {{password}}
               <button class="btn btn-lg btn-success btn-block text-uppercase" type="submit">Accedi</button>
               <hr class="my-4">
               
@@ -37,27 +39,148 @@
 
 <script>
 
+import axios from 'axios'
 import {mapMutations} from 'vuex'
 
 export default {
     name: 'Login',
     data (){
       return {
-
+        email : "",
+        phone : "",
+        password : "",
+        emailOrPhone : "",
+        readyEmail : false,
+        readyPhone : false,
+        emailOk : false,
+        passwordOk : false,
+        phoneOk : false
       }
     },
+
+    watch : {
+      email : function(){
+        let i
+        const dim = this.email.length
+        let counter = 0
+        if(this.email[0] == '@' || this.email[dim - 1] == '@') this.emailOk = false
+        for(i=0;i<dim;i++){
+          if(this.email[i] == '@' && counter == 0){
+            counter++
+          }
+        }
+        if(counter > 1) this.emailOk = false
+        this.emailOk = true
+      },
+
+      phone : function(){
+        if(this.phone.length != 10){
+          this.phoneOk = false
+        }
+        this.phoneOk = true
+      },
+
+      password : function(){
+        const len = this.password.length 
+        if(len < 5 || len > 1024) {
+          this.passwordOk = false
+        }
+        this.passwordOk = true
+      },
+
+      emailOk : function(){
+        if(this.emailOk && this.passwordOk && !this.phoneOk){
+          this.readyEmail = true
+          this.emailOrPhone = this.email
+        }
+        this.readyEmail = false
+      },
+
+      phoneOk : function(){
+        if(!this.emailOk && this.passwordOk && this.phoneOk){
+          this.readyPhone = true
+          this.emailOrPhone = this.phone
+        } 
+        this.readyPhone = false
+      }
+    },
+
     methods : {
       ...mapMutations([
         'setLogged'
       ]),
-      
-      /* email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(1024).required()*/
+    
+      async accedi() {
+        // Controllo per vedere se /email o /phone
+        if(this.emailOk && !this.phoneOk){
+          const result = await axios.post('localhost:8081/auth/email', {
+            body : {
+              email : this.email,
+              password : this.password
+            }
+          })
+          if(!result) console.log('Errore....')
+          console.log(result)
+        }
 
-      chechEmail(){
-          if(!isNaN(parseInt(document.getElementById('inputEmailOrPhone ').value))) return false
-          return true 
+        else if(!this.emailOk && this.phoneOk){
+          const result = await axios.post('localhost:8081/auth/phone', {
+            body : {
+              phone : this.phone,
+              password : this.password
+            }
+          })
+          if(!result) console.log('Errore....')
+          console.log(result)
+        }
+
+        // Settare a true la variabile logged nello store
+
       }
     },
 }
 </script>
+
+
+<style scoped>
+
+.card-signin {
+  border: 0;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.card-signin .card-title {
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  height: auto;
+  border-radius: 2rem;
+}
+
+
+</style>
