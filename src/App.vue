39d9ba7e8c1323517b034ac0a98a739b00d39d9d
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-29 17:15:51 +0200

    started

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!--menu-->
+
+  <!--Navbar-->
+  <nav class="navbar navbar-light navbar-1 black bg-success">
+
+  <!-- Navbar brand -->
+  <a class="navbar-brand" href="#"><img src="./assets/logo.svg"> DIANA</a>
+
+  <!-- Collapse button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
+    aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+  <!-- Collapsible content -->
+  <div class="collapse navbar-collapse" id="navbarSupportedContent15">
+
+    <!-- Links -->
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <!-- Links -->
+
+  </div>
+  <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
+    <!-- -->
+
+    <!-- PARTE CHE DEVE VARIARE-->
+    <div class="container-fluid ">
     <router-view/>
+    </div>
+
+    <!-- --->
+
+    <!-- footer-->
   </div>
 </template>
 
