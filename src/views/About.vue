a07bc0bba7c6877b1d74e3098bb740c0487fe793
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-04-30 21:02:18 +0200

    updated with logut

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="removeCredentials()" type="button" class="btn btn-sm btn-success btn-block text-uppercase">Logout</button>
   </div>
 </template>
+
+<script>
+import {mapMutations} from 'vuex'
+
+export default {
+  name: "About",
+
+  computed : {
+
+  },
+
+  methods : {
+    removeCredentials(){
+      this.$store.commit('setLogged', false)
+      this.$store.commit('setToken', '')
+      this.$store.commit('setType', '')
+      
+      localStorage.removeItem('email')
+      localStorage.removeItem('phone')
+      localStorage.removeItem('password')
+      localStorage.removeItem('type')
+
+      this.$router.push('/')
+    },
+
+    ...mapMutations([
+        'setLogged',
+        'setToken',
+        'setType'
+      ])
+  }
+}
+</script>
