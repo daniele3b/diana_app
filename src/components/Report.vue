a169dec86580f84f6399c0c5812a4fc3cbebae5c
Mario <cavaiola.1794586@studenti.uniroma1.it>
2020-05-02 17:52:14 +0200

    Report daniee

---
+++ src/components/Report.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="card-columns">
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">DANIELE</h5>
+      <p class="card-text">DANIELE</p>
+    </div>
+  </div>
+ </div>
+</template>
+
+<script>
+export default {
+    
+
+}
+</script>
