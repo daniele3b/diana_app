c0d9802c4b9af96e8bb12cb6d785060af37548bf
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-05-03 17:39:29 +0200

    nothing

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="card" style="height:500px;">
+  <div class="card" style="height:770px;">
     <div class="card-body">
       <div class="container">
-        <div class="row" style=height:250px;>
-          <div class="google-map" ref="googleMap"></div>
+        <div class="row" style=height:385px;>
+          <GmapMap
+            :center="{lat:41.9109, lng:12.6818}"
+            :zoom="9"
+            map-type-id="terrain"
+            style="width: 500px; height: 300px"
+          >
+          <GmapMarker
+            :key = sensor.lat
+            v-for="sensor in info" 
+            :position="google && new google.maps.LatLng(sensor.lat, sensor.lng)"
+            :clickable="true"
+            :draggable="true"
+          />
+          
+           
+          </GmapMap>
         </div>
 
         <hr>
 
-        <div class="row" style=height:250px;>
-          INFO SENSORI
+        <div class="row" style=height:385px;>
+          {{info}}
         </div>
       </div>
     </div>
@@ -19,37 +34,78 @@
 
 
 <script>
-import GoogleMapsApiLoader from 'google-maps-api-loader'
+import axios from 'axios'
+import {gmapApi} from 'vue2-google-maps'
 
 export default {
-  props: {
-    mapConfig: Object,
-    apiKey: String,
+  computed: {
+    google: gmapApi
   },
 
-  data() {
+
+  data () {
     return {
-      google: null,
-      map: null
+      center : {lat : 0.0, lng : 0.0},
+      info : [],
+      sensors : []
     }
   },
 
-  async mounted() {
-    const googleMapApi = await GoogleMapsApiLoader({
-      apiKey: this.apiKey
-    })
-    this.google = googleMapApi
-    this.initializeMap()
+  beforeCreate(){
+
   },
 
-  methods: {
-    initializeMap() {
-      const mapContainer = this.$refs.googleMap
-      this.map = new this.google.maps.Map(mapContainer, this.mapConfig)
+  mounted:
+    function getDataFromSensors(){
+      console.log("MOUNTED -> DIM="+this.sensors.length)
+      axios({
+            method: 'get',
+            url: 'http://localhost:8081/chemical_agents',
+            headers: {
+              "x-diana-auth-token": localStorage.token
+            }
+          })
+          .then((response) => {
+            let i=0
+            const dim = response.data.length
+            for(i=0;i<dim;i++){
+              this.info.push({
+                value : response.data[i].value,
+                sensor : response.data[i].sensor,
+                uid : response.data[i].uid,
+                lat : response.data[i].lat,
+                lng : response.data[i].long,
+              })
+
+              if(!this.trovato(this.sensors, response.data[i])){
+                this.sensors.push({
+                  lat : response.data[i].lat,
+                  lng : response.data[i].long
+                })
+              }
+            }
+
+          })
+          .catch((error) => {
+              console.log(error)
+          })
+          console.log(this.info)
+          console.log(this.sensors)
+      },
+    
+    methods : {
+      trovato(arr, sensor){
+        let dim = arr.length
+        let i
+        for(i=0;i<dim;i++){
+          if(arr[i].lat == sensor[i].lat && arr[i].lng == sensor[i].long) return true
+        }
+        return false
+      }
     }
-  }
-  
-}
+    
+    }
+
 </script>
 
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+require('dotenv').config()
 
 Vue.config.productionTip = false
 
@@ -10,3 +13,10 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDHqV6lgLeqIRoVRBh47mQVjwI-mX9891E',
+    libraries: 'places'
+  },
+})
