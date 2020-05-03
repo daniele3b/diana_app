56076045899e32d57f158be8341b4e05c9563861
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-05-03 09:32:14 +0200

    map updated

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,70 +1,57 @@
 <template>
-    <div class="card" style="height:500px;">
-  <div class="card-header">
-    Featured
+  <div class="card" style="height:500px;">
+    <div class="card-body">
+      <div class="container">
+        <div class="row" style=height:250px;>
+          <div class="google-map" ref="googleMap"></div>
+        </div>
+
+        <hr>
+
+        <div class="row" style=height:250px;>
+          INFO SENSORI
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
 </template>
 
 
 
 <script>
 export default {
-    name : "Map"
+  props: {
+    mapConfig: Object,
+    apiKey: String,
+  },
+
+  data() {
+    return {
+      google: null,
+      map: null
+    }
+  },
+
+  async mounted() {
+    const googleMapApi = await GoogleMapsApiLoader({
+      apiKey: this.apiKey
+    })
+    this.google = googleMapApi
+    this.initializeMap()
+  },
+
+  methods: {
+    initializeMap() {
+      const mapContainer = this.$refs.googleMap
+      this.map = new this.google.maps.Map(mapContainer, this.mapConfig)
+    }
+  }
+  
 }
 </script>
 
 
 
 <style scoped>
-
-.card-signin {
-  border: 1;
-  border-radius: 1rem;
-  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
-}
-
-.card-signin .card-title {
-  font-weight: 300;
-  font-size: 1.5rem;
-}
-
-.card-signin .card-body {
-  padding: 2rem;
-}
-
-.form-signin {
-  width: 100%;
-}
-
-.form-signin .btn {
-  font-size: 80%;
-  border-radius: 5rem;
-  letter-spacing: .1rem;
-  font-weight: bold;
-  padding: 1rem;
-  transition: all 0.2s;
-}
-
-.form-label-group {
-  position: relative;
-  margin-bottom: 1rem;
-}
-
-.form-label-group input {
-  height: auto;
-  border-radius: 2rem;
-}
-
-.my-4 {
-  border : 1;
-  border-color : grey;
-}
-
 
 </style>
