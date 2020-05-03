302255b9254e8cbc65954f5e38b987e04bd20f85
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-03 09:18:23 +0200

    report

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -1,9 +1,58 @@
 <template>
- <div class="card text-white bg-primary mb-3" >
-  <div class="card-header">Header</div>
+ <div class="card   mt-1" >
+  <div class="card-header">Segnalazioni</div>
   <div class="card-body">
-    <h5 class="card-title">Primary card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="col-md-12">
+    <div class="table-responsive">
+    <table id="mytable" class="table table-bordred table-striped">
+        <thead>
+          <th>CF</th>
+          <th>Categoria</th>
+          <th>Data</th>
+          <th>Stato</th>
+        </thead>
+      <tbody>
+    
+            <tr>
+                <td>BFLDNL98T02H501H</td>
+                <td>Rifiuti</td>
+                <td>3/5/2020</td>
+                <td>PRESA IN CARICO</td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Detail"><button class="btn btn-success btn-xs" data-title="Detail" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+            <tr>
+                <td>BFLDNL98T02H501H</td>
+                <td>Rifiuti</td>
+                <td>3/5/2020</td>
+                <td>PRESA IN CARICO</td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Detail"><button class="btn btn-success btn-xs" data-title="Detail" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+            <tr>
+                <td>BFLDNL98T02H501H</td>
+                <td>Rifiuti</td>
+                <td>3/5/2020</td>
+                <td>PRESA IN CARICO</td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Detail"><button class="btn btn-success btn-xs" data-title="Detail" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+            <tr>
+                <td>BFLDNL98T02H501H</td>
+                <td>Rifiuti</td>
+                <td>3/5/2020</td>
+                <td>PRESA IN CARICO</td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Detail"><button class="btn btn-success btn-xs" data-title="Detail" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+            </tr>
+      </tbody>
+    </table>
+    </div>
+  </div>
   </div>
 </div>
 </template>
@@ -18,7 +67,55 @@ export default {
 
 
 
+
 <style scoped>
+
+.card-signin {
+  border: 1;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.card-signin .card-title {
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  height: auto;
+  border-radius: 2rem;
+}
+
+.my-4 {
+  border : 1;
+  border-color : grey;
+}
+
+#mytable{
+  font-size: 8;
+}
 
 
 
