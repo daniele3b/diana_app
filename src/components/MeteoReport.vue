8c279b10914c93f02541ba038a78acd813508990
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-04 15:47:42 +0200

    added component

--- src/components/MeteoReport.vue
+++ src/components/MeteoReport.vue
@@ -1,8 +1,12 @@
 <template>
  <div>
     <div class="row">
-     
+     <div class="col">
             <Meteo></Meteo>
+      </div>
+      <div class="col">
+        <UV>  </UV>
+        </div>
         
     </div>
     <div class="row">
@@ -17,12 +21,14 @@
 import Meteo from './Meteo.vue'
 
 import Report from './Report.vue'
+import UV from './UV.vue'
 
 export default {
   name: 'MeteoReport',
   components: {
     Meteo,
     Report,
+    UV
 
   }
 }
