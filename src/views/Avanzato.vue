df2f004a6eed69296e8e51a0cb039ae5f1d09dd3
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-06 13:57:32 +0200

    menu avanzato

--- src/views/Avanzato.vue
+++ src/views/Avanzato.vue
@@ -4,29 +4,32 @@
         <div class="col">
              <router-link to="/report_storico">
          <button   id="im1"></button> 
-           
-                
-        
           </router-link>
         </div>
         <div class="col">
-            <router-link to="/">     <img src="../assets/menu_trasporti2.png" height="250px" width="300px">
-    </router-link>
+            <router-link to="/report_storico">
+         <button   id="im2"></button> 
+          </router-link>
+
         </div>
         <div class="col">
-            <router-link to="/"> <img src="../assets/storico.png"> </router-link>
+            <router-link to="/report_storico">
+         <button   id="im3"></button> 
+          </router-link>
         </div>
     </div>
      <div class="row mt-3">
-        <div class="col">
-            <router-link to="/"> <img src="../assets/storico.png"> </router-link>
+        <div class="col-4 d-none d-sm-block">
+          <router-link to="/report_storico">
+         <button   id="im4"></button> 
+          </router-link>
         </div>
-        <div class="col">
-            <router-link to="/"> <img src="../assets/storico.png"> </router-link>
+        <div class="col d-block d-sm-none">
+          <router-link to="/report_storico">
+         <button   id="im4"></button> 
+          </router-link>
         </div>
-        <div class="col">
-            <router-link to="/"> <img src="../assets/storico.png"> </router-link>
-        </div>
+       
     </div>
 
   </div>
@@ -57,6 +60,57 @@ export default {
 
 #im1:hover{
   background-image:url('../assets/menu_trasporti2.png');
+   
+    
+}
+
+#im2{
+
+    height:250px;  
+    width:300px;
+    background-image:url('../assets/menu_analisi.png');
+     background-size: cover;
+     border:0;
+     background-color: transparent;
+
+}
+
+#im2:hover{
+  background-image:url('../assets/menu_analisi2.png');
+   
+    
+}
+
+#im3{
+
+    height:250px;  
+    width:300px;
+    background-image:url('../assets/menu_annunci.png');
+     background-size: cover;
+     border:0;
+     background-color: transparent;
+
+}
+
+#im3:hover{
+  background-image:url('../assets/menu_annunci2.png');
+   
+    
+}
+
+#im4{
+
+    height:250px;  
+    width:300px;
+    background-image:url('../assets/menu_ins.png');
+     background-size: cover;
+     border:0;
+     background-color: transparent;
+
+}
+
+#im4:hover{
+  background-image:url('../assets/menu_ins2.png');
    
     
 }
