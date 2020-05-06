0ad8eb4798942e565b1806380f47cd844527f32a
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-06 12:36:12 +0200

    menu figo

--- src/views/Avanzato.vue
+++ src/views/Avanzato.vue
@@ -3,16 +3,15 @@
     <div class="row mt-3">
         <div class="col">
              <router-link to="/report_storico">
-         
-        <img src="../assets/menu_trasporti.png" height="250px" width="300px">
-   
+         <button   id="im1"></button> 
+           
                 
-          
         
           </router-link>
         </div>
         <div class="col">
-            <router-link to="/"> <img src="../assets/storico.png"> </router-link>
+            <router-link to="/">     <img src="../assets/menu_trasporti2.png" height="250px" width="300px">
+    </router-link>
         </div>
         <div class="col">
             <router-link to="/"> <img src="../assets/storico.png"> </router-link>
@@ -42,3 +41,23 @@ export default {
 
 }
 </script>
+
+<style scoped>
+
+#im1{
+
+    height:250px;  
+    width:300px;
+    background-image:url('../assets/menu_trasporti.png');
+     background-size: cover;
+     border:0;
+     background-color: transparent;
+
+}
+
+#im1:hover{
+  background-image:url('../assets/menu_trasporti2.png');
+   
+    
+}
+</style>
