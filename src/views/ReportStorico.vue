2c4c3644dd405d9a5421713a2d61d7fa692a4f1d
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-07 10:28:27 +0200

    rrdy

--- src/views/ReportStorico.vue
+++ src/views/ReportStorico.vue
@@ -650,6 +650,13 @@ export default {
                         this.reports.push(this.app[i])
                 }
 
+                //se cat ini e fine
+             if(this.CF2filter==''&& this.status2filter==''&&this.data_inizio!=''&&this.data_fine!=''&&this.cat2filter!='')
+                {
+                     if(this.app[i].date<=this.data_fine&&this.app[i].date>=this.data_inizio&&this.app[i].category==(this.cat2filter.toUpperCase()))
+                        this.reports.push(this.app[i])
+                }
+
          }
          console.log(this.reports)
           this.filter=false
