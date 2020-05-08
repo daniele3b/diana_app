1fd8d25ac149309c966dfbb4d0901cab25c0d617
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-08 19:17:53 +0200

    layout ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,36 @@
 <template>
  <div class="container-fluid">
-<div class="row" id="rigaesterna">
-  <div class="row " id="rigainterna">
+<div class="row mt-2" id="rigaesterna">
+  <div class="col-12">
+  <div class="row mt-1 mb-1 " id="rigainterna">
     <div class="col-12">
-          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img class="d-block w-100" src="../assets/slide1.jpg" alt="First slide">
+      <center>
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block " src="../assets/slide1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block " src="../assets/slide1.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block " src="../assets/slide1.jpg" alt="Third slide">
+                </div>
               </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="../assets/slide1.jpg"  alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="../assets/slide1.jpg"  alt="Third slide">
-              </div>
+              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
             </div>
-  
+            </center>
           </div>
-    </div>
   </div>
 
+</div>
   </div>
 
   </div>
@@ -44,5 +50,9 @@ export default {
 
 #rigaesterna{
   background-color: black;
+}
+
+#rigainterna{
+  background-color: yellow;
 }
 </style>
