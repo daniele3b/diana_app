d98ab8900aa821bcc989226a29f147030964577c
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-05-13 19:52:45 +0200

    .

--- src/components/GraficoStazione.vue
+++ src/components/GraficoStazione.vue
@@ -1,30 +1,47 @@
 <template>
     <div>
         
-        <div v-for ="(agente, index) in dati_stazione" :key="index">
-          {{agente}} <br>
+        <div v-for="agente in dati_stazione" :key = agente>
+            {{agente}}
         </div>
-        
+
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { Bar } from 'vue-chartjs'
 
 export default {
+    extends: Bar,
 
     name: "GraficoStazione",
     
     data(){
       return {
         dati_stazione : [],
+        clicked : false
       }
     },
 
     beforeCreate(){
-    
-      const url = 'http://localhost:8081/chemical_agents/current/'+this.$store.getters.getStazione
+      const oggi = new Date()
 
+      let mese = oggi.getMonth() + 1
+      let giorno = oggi.getDate()
+      let anno = oggi.getFullYear()
+
+      let giorno_inizio = giorno - 7
+        
+      if(mese < 10)
+        mese = '0' + mese
+      if(giorno < 10)
+        giorno = '0' + giorno
+      if(giorno_inizio < 10)
+        giorno_inizio = '0' + giorno_inizio
+
+      const url = 'http://localhost:8081/chemical_agents/filter/date/'+this.$store.getters.getStazione+'/'+anno+'-'+mese+'-'+giorno_inizio+'/'+anno+'-'+mese+'-'+giorno
+      
       axios({
         method: 'get',
         url: url,
@@ -35,12 +52,19 @@ export default {
       })
 
       .then((response) => {
+        alert("QUI")
         this.dati_stazione = response.data
       })
     },
 
+    mounted(){
+//      this.renderChart(data, options)
+    },
+
     methods : {
-      
+      faiCose(){
+        
+      }
     },
 }
 
