bfe46f43c5ea1e9295b2a0dbe8a1fc9a25c4dec2
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-05-13 15:24:41 +0200

    .

--- src/views/GraficoStazione.vue
+++ src/views/GraficoStazione.vue
@@ -11,8 +11,29 @@ export default {
     
     data(){
       return {
-          
+        dati_stazione : [],
       }
+    },
+
+    beforeCreate(){
+
+      // VEDERE SE NEL LOCAL STORAGE C'E' L'ID DELLA STAZIONE
+
+      axios({
+        method: 'get',
+        url: 'http://localhost:8081/chemical_agents/current/9343',
+        headers: {
+          "x-diana-auth-token": localStorage.token
+        }
+
+        .then((response) => {
+          
+        })
+
+        .catch((error) => {
+          console.log(error)
+        })
+      })
     },
 
     methods : {
