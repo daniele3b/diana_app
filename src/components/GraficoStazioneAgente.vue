680823067868521419396e29208b8945a292a8fa
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-13 19:04:53 +0200

    fixed

--- src/components/GraficoStazioneAgente.vue
+++ src/components/GraficoStazioneAgente.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         GRAFICO STAZIONE AGENTE
+        {{stat}}
+        {{agent}}
     </div>
 </template>
 
@@ -12,12 +14,37 @@ export default {
     data(){
       return {
         dati_stazione : [],
+        stat:'',
+        agent:''
       }
     },
 
-    beforeCreate(){
+computed: {
+    checkStazione () {
+    return this.$store.getters.getStazione
+  },
+  checkAgente(){
+    return this.$store.getters.getAgente
+  }
+},
 
- 
+watch:{
+    checkStazione(value) {
+       this.stat=value
+       this.updateData()
+    },
+    checkAgente(value)
+    {
+      this.agent=value
+      this.updateData()
+    }
+},
+
+
+beforeCreate(){
+
+        this.stat=this.$store.getters.getStazione
+        this.agent=this.$store.getters.getAgente
         let data=new Date()
 
         let month=data.getMonth()+1
@@ -48,10 +75,47 @@ export default {
         })
      
     },
+    methods:
+    {
+      updateData:function()
+      {
 
-    methods : {
-      
-    },
+        this.stat=this.$store.getters.getStazione
+        this.agent=this.$store.getters.getAgente
+        let data=new Date()
+
+        let month=data.getMonth()+1
+        let day=data.getDate()
+        let year=data.getFullYear()
+
+        let day_start=day-7
+        
+        if(month<10)
+          month='0'+month
+        if(day<10)
+          day='0'+day
+        if(day_start<10)
+          day='0'+day
+
+      axios({
+        method: 'get',
+        url: 'http://localhost:8081/chemical_agents/filter/date/'+year+'-'+month+'-'+day_start+'/'+year+'-'+month+'-'+day+'/type/'+this.agent+'/'+this.stat,
+        headers: {
+          "x-diana-auth-token": localStorage.token
+        }})
+        .then((response) => {
+          console.log(response)
+        })
+
+        .catch((error) => {
+          console.log(error)
+        })
+      }
+
+    }
+    
+
+
 }
 
 </script>
