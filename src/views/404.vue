beb9f876b5dc530af295d6038bee06bd5188fcb0
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-24 13:59:32 +0200

    fix

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,17 +1,36 @@
 <template>
  
  <div class="container">
-     <div class="row">
-         <div class="col">
-             404
-             </div>
-     </div>
+    <div class="row">
+        <div class="col">
+            404 NOT FOUND
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+             PER DIANA!
+        </div>
+    </div>
 </template>
 
 <script>
 
 export default {
   name: '404',
+
+  mounted()
+  {
+      setTimeout(this.cambiaPagina,5000)
+  },
+  methods:
+  {
+      cambiaPagina: function()
+      {
+
+             this.$router.push('/login')
+      }
+
+  }
 
 }
 </script>
