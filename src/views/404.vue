6e200365e4fa39a52065e17be338219bbe912916
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-24 13:56:44 +0200

    pushed

---
+++ src/views/404.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+ <div class="container">
+     <div class="row">
+         <div class="col">
+             404
+             </div>
+     </div>
+</template>
+
+<script>
+
+export default {
+  name: '404',
+
+}
+</script>
+
